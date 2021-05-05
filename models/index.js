59141cf5715b24b96c8f5d3588f4b6e5ccb0ab20
Brandi Murray <pritchettb14@gmail.com>
2021-05-04 22:07:41 -0400

    homepage displays when server ran (css not working)

--- models/index.js
+++ models/index.js
@@ -3,13 +3,7 @@ const Tracks = require('./Tracks');
 const Artists = require('./Artists');
 
 
-// Tracks.hasMany(Artists, {
-//   foreignKey: 'gallery_id',
-// });
 
-// Artists.belongsTo(Tracks, {
 
-//   foreignKey: 'gallery_id',
-// });
 
 module.exports = { User, Tracks, Artists };
