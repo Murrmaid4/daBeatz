ab4dc779c9c2763a6e240a0b2a5e5af98842cab3
T Heier <trentheier@gmail.com>
2021-05-06 14:14:16 -0400

    added a route to the artists table from our db so now we can create the get requests to the artist table data

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const dbTracks = require('./dbTracks')
+const dbArtists = require('./dbArtists')
 
 router.use('/users', userRoutes);
-router.use('/data', dbTracks)
+router.use('/data', dbTracks);
+router.use('/artistData', dbArtists);
 
 
 module.exports = router;
