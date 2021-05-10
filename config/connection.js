d2e92b7174ff0779cc9c59f274b5ea821b7ef311
Brandi Murray <pritchettb14@gmail.com>
2021-05-10 15:20:36 -0400

    procfile for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
+  process.env.JAWSDB_URL
 );
 
 module.exports = sequelize;
